5ab615465c0a8600a5b16523ba9b19292a20c768
Jihye Seo <23goqkfkrl@duksung.ac.kr>
2022-12-18 21:22:17 +0900

    홈페이지 완성

--- items/views.py
+++ items/views.py
@@ -11,7 +11,6 @@ from django.db.models import Q
 # Create your views here.
 class PostList(ListView):
     model = Post
-    ordering = '-pk'
     paginate_by = 4
 
     def get_context_data(self, **kwargs):
